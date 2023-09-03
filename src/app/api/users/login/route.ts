a4be578acb7f12a718e75c67a32c28a7c21ec6f3
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2023-09-03 20:30:19 +0530

    next.js first app is ready now

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,58 @@
+/** @format */
+
+import { connect } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import UserModel from "@/models/userModel";
+import { use } from "react";
+var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+connect();
+
+export let POST = async (request: NextRequest) => {
+  try {
+    let reqBody = await request.json();
+    let { email, password } = reqBody;
+    console.log(reqBody);
+
+    // fine the user is exists or not
+    let user = await UserModel.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "user does not exist" },
+        { status: 400 }
+      );
+    }
+
+    // hash the password
+    let validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    let tokenData = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+
+    //create token
+    let token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
+
+    let response = NextResponse.json({
+      message: "Login successful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
+
+    // return NextResponse.json({
+    //   message: "Login successful",
+    //   success: true,
+    // }).cookies.set(token);
+  } catch (error: any) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+};
